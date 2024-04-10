eb29c7fc7928ada03170dfca2d1c3d0cdbd02219
Damien <damien.harle@gmail.com>
2024-04-10 16:46:44 +0200

    Death

---
+++ UE_DeathRunner_P4/Source/UE_DeathRunner_P4/Cpp_DeathScreen.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Cpp_DeathScreen.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_DEATHRUNNER_P4_API UCpp_DeathScreen : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
